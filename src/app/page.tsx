23cddf8e5e13b2d24c7d5089b1d3e86b4faa4b96
Charles Harris <char5653@uni.sydney.edu.au>
2024-01-05 22:53:21 +1100

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ import StravaTracker from "@/components/StravaTracker";
 import { getActivities, getAthlete } from "@/lib/strava";
 
 export default async function Home() {
-  const data = await getAthlete();
   const activities = await getActivities();
 
   return (
     <main>
-      <Navbar data={data} />
       {activities && (
         <>
           <ActivityList activities={activities} />
