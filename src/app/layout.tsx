bb9f47fb35f7fe32d01481de421353c61ca0285b
Charles Harris <char5653@uni.sydney.edu.au>
2024-01-09 15:37:39 +1100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,14 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>
         <Navbar data={data} />
         {children}
