3a4862240481edda1f514f5142b23066f08b3e25
Charles Harris <char5653@uni.sydney.edu.au>
2024-01-10 16:04:09 +1100

    reset head component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,14 +20,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-      </head>
       <body className={inter.className}>
         <Navbar data={data} />
         {children}
